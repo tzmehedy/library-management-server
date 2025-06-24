e9b2db69089a58730f5e28b938d33d9a91231983
unknown <mehedytouhidurzaman@gmail.com>
2025-06-24 16:51:17 +0600

    The mongodb database is connected

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export default {
-    port: process.env.PORT
+    port: process.env.PORT,
+    database_url: process.env.DATABASE_URL
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { Server } from "http"
 import app from "./app"
 import config from "./config"
+import mongoose from "mongoose"
 let server:Server
 
-function main(){
+async function main(){
     try{
+        await mongoose.connect(`${config.database_url}`)
+        console.log("The library server is running")
         server = app.listen(config.port,()=>{
             console.log(`The server is running on the port ${config.port}`)
         })
