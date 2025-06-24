8b019a17abdaf72313934f220082f4fe7980e18d
unknown <mehedytouhidurzaman@gmail.com>
2025-06-25 02:23:05 +0600

    The book is updated by id is completed

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,46 +1,42 @@
 import { Request, Response } from "express";
 import Book from "../models/book.model";
 
-const createBook = async(req:Request, res:Response)=>{
-    try{
-        const payload = req.body;
-        const book = await Book.create(payload);
-        res.json({
-          success: true,
-          message: "Book created successfully",
-          data: book,
-        });
-    }
-    catch(err:any){
-      const modifiedError = {
-        errors: err.errors,
-        name: err.name
-      } 
-        res.json({
-          success: false,
-          message: err._message,
-          error: modifiedError,
-        });
-    }
-}
+const createBook = async (req: Request, res: Response) => {
+  try {
+    const payload = req.body;
+    const book = await Book.create(payload);
+    res.json({
+      success: true,
+      message: "Book created successfully",
+      data: book,
+    });
+  } catch (err: any) {
+    const modifiedError = {
+      errors: err.errors,
+      name: err.name,
+    };
+    res.json({
+      success: false,
+      message: err._message,
+      error: modifiedError,
+    });
+  }
+};
 
+const getAllBooks = async (req: Request, res: Response) => {
+  try {
+    const filter = req.query.filter;
+    const sortBy = req.query.sortBy;
+    const sort = req.query.sort;
+    const limit = parseInt(`${req.query.limit}`);
 
-
-const getAllBooks = async(req:Request, res:Response)=>{
-  try{
-    const filter = req.query.filter
-    const sortBy = req.query.sortBy
-    const sort = req.query.sort
-    const limit = parseInt(`${req.query.limit}`)
-
-    console.log(filter, sortBy, sort, limit)
-    let books = []
+    console.log(filter, sortBy, sort, limit);
+    let books = [];
     if (filter || sortBy || sort || limit) {
       books = await Book.find({ genre: `${filter}` })
         .sort({ sortBy: sort === "asc" ? 1 : -1 })
         .limit(limit);
-    }
-    else{
+    } else {
       books = await Book.find().limit(10);
     }
     res.json({
@@ -48,8 +44,7 @@ const getAllBooks = async(req:Request, res:Response)=>{
       message: "Books retrieved successfully",
       data: books,
     });
-
-  }catch(err:any){
+  } catch (err: any) {
     const modifiedError = {
       errors: err.errors,
       name: err.name,
@@ -59,21 +54,19 @@ const getAllBooks = async(req:Request, res:Response)=>{
       message: err._message,
       error: modifiedError,
     });
-
   }
+};
 
-}
-
-const getBookById = async(req:Request, res:Response)=>{
-  try{
-    const bookId = req.params.bookId
-  const book = await Book.findById(bookId)
-  res.json({
-    success: true,
-    message: "Books retrieved successfully",
-    data: book,
-  });
-  }catch(err:any){
+const getBookById = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.bookId;
+    const book = await Book.findById(bookId);
+    res.json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: book,
+    });
+  } catch (err: any) {
     const modifiedError = {
       errors: err.errors,
       name: err.name,
@@ -83,10 +76,30 @@ const getBookById = async(req:Request, res:Response)=>{
       message: err._message,
       error: modifiedError,
     });
-
   }
+};
 
+const updateBook = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.bookId;
+    const payload = req.body;
+    const book = await Book.findByIdAndUpdate(bookId, payload, { new: true });
+    res.json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: book,
+    });
+  } catch (err: any) {
+    const modifiedError = {
+      errors: err.errors,
+      name: err.name,
+    };
+    res.json({
+      success: false,
+      message: err._message,
+      error: modifiedError,
+    });
+  }
+};
 
-}
-
-export { createBook, getAllBooks, getBookById }
+export { createBook, getAllBooks, getBookById, updateBook };
--- src/routers/book.routes.ts
+++ src/routers/book.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createBook, getAllBooks, getBookById } from "../controllers/book.controller";
+import { createBook, getAllBooks, getBookById, updateBook } from "../controllers/book.controller";
 
 export const bookRouter = Router()
 
 bookRouter.post("/", createBook)
 bookRouter.get("/:bookId", getBookById)
+bookRouter.put("/:bookId", updateBook)
 bookRouter.get("/", getAllBooks)
 
 
